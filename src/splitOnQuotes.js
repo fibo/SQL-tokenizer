b5c4d5eb8cebe5b2ab39a7b24dcd6618f159c81e
fibo <casati_gianluca@yahoo.it>
2018-02-14 16:16:20 +0100

    escape quote

--- src/splitOnQuotes.js
+++ src/splitOnQuotes.js
@@ -1,57 +1,53 @@
 function splitOnQuotes (sql: string): Array<string> {
-  let tokens = []
+  let blocks = []
 
   // No SQL statement can start with a quoted word.
-  let isQuoted = false
-  let currentQuotedValue = ''
+  let isInsideQuotes = false
 
   sql.split("'").forEach(function (currentValue, index, array) {
-    let nextValue
-
-    if (isQuoted) {
-      // Concatenate currentValue with currentQuotedValue, if any, cause
-      // previous value could be quoted as well.
-      if (currentQuotedValue === '') {
-        currentQuotedValue = currentValue
-      } else {
-        currentQuotedValue += "'" + currentValue
-      }
-
-      // Return if it is last element, otherwise nextValue will be undefined.
-      if (index === array.length - 1) {
-        tokens.push("'" + currentQuotedValue)
-
-        return
-      } else {
-        // A single quote can be escaped by another single quote.
-        //
-        // > var sql = "select 'O''Reilly';"
-        // > sql.split(/'/)
-        // [ 'select ', 'O', '', 'Reilly', ';' ]
-
-        nextValue = array[index + 1]
-
-        // If currentValue is quoted and two consecutive single quotes are found,
-        // return to avoid toggling the isQuoted flag, hence nextValue will
-        // be considered quoted and concatenated to currentQuotedValue.
-        //
-        // Otherwise just push the quoted token found.
-        if (nextValue === '') {
-          return
-        } else {
-          tokens.push("'" + currentQuotedValue + "'")
-        }
-      }
+    if (isInsideQuotes) {
+      isInsideQuotes = false
+      blocks.push(`'${currentValue}'`)
     } else {
-      tokens.push(currentValue)
+      isInsideQuotes = true
+      blocks.push(currentValue)
     }
-
-    // Toggle the isQuoted flag:
-    // if currentValue is quoted the next one will not, and viceversa.
-    isQuoted = !isQuoted
   })
 
-  return tokens
+  // Every empty block is an escaped quote.
+  // select 'O ''Reilly' from mytable
+  // [ 'select ', '\'O \'', '', '\'Reilly\'', ' from mytable' ]
+  return blocks.reduce(function (accumulator, currentValue, currentIndex, array) {
+    const isEmptyString = currentValue === ''
+    const isLastOne = currentIndex === array.length - 1
+
+    if (currentIndex === 0) {
+      accumulator = array.slice(0)
+    }
+
+    // If statement end with a quote there will be a final emptyString, to be removed.
+    if (isEmptyString && isLastOne) {
+      return accumulator.slice(0, array.length - 1)
+    }
+
+    if (isEmptyString) {
+      let numEmptyValues = 0
+
+      for (let i = currentIndex; i < array.length - 1; i++) {
+        if (array[i] === '') {
+          numEmptyValues++
+        } else {
+          // Remove empty strings.
+          accumulator.splice(currentIndex, numEmptyValues)
+          // Merge current and next value.
+          accumulator[currentIndex - 1] += accumulator.splice(currentIndex, 1).join('')
+          break
+        }
+      }
+    }
+
+    return accumulator
+  }, [])
 }
 
 module.exports = splitOnQuotes
--- test/splitOnQuotes.js
+++ test/splitOnQuotes.js
@@ -4,8 +4,9 @@ const split = require('../src/splitOnQuotes')
 
 test('splitOnQuotes', (t) => {
   t.deepEqual(split('select'), ['select'], 'single word')
-  t.deepEqual(split("select 'hello world'"), ['select ', "'hello world'"], 'single quotes')
-  // TODO t.deepEqual(split("select 'O ''Reilly'"), ['select ', "'O''Reilly'"], 'escape quotes')
+  t.deepEqual(split("select 'hello world' from mytable"), ['select ', "'hello world'", ' from mytable'], 'single quotes')
+  t.deepEqual(split("select 'O''Reilly' from mytable"), ['select ', "'O''Reilly'", ' from mytable'], 'escape quotes')
+  t.deepEqual(split("select 'hello world'"), ['select ', "'hello world'"], 'single quotes, last char is a quote')
 
   t.end()
 })
